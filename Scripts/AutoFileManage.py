a0c3d0e0fa337905a27c5b224a4d43f896e03291
Chaz42 <forteetwo@hotmail.com>
2020-01-09 05:10:44 -0500

    Working GUI, Run button executes sort

--- Scripts/AutoFileManage.py
+++ Scripts/AutoFileManage.py
@@ -1,24 +1,26 @@
 import tkinter as tk
+import SortDownloads
+from tkinter import ttk
 
 LARGE_FONT = ("Helvetica", 16)
 
 class ConfigureTool(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
 
+        tk.Tk.wm_title(self, "Automated File Manager")
+
+        container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
-        
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        frame = StartPage(container, self)
-
-        self.frames[StartPage] = frame
-
-        frame.grid(row=0, column = 0, sticky="nsew")
+        for F in (StartPage, ConfigPage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column = 0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -27,11 +29,35 @@ class ConfigureTool(tk.Tk):
         frame.tkraise()
 
 class StartPage(tk.Frame):
-
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text ="Hello", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+
+        label1 = tk.Label(self, text ="Hello", font=LARGE_FONT)
+        label1.pack(pady=10)
+
+        btnOpen = ttk.Button(self, text="Open Config", command=lambda: controller.show_frame(ConfigPage))
+        btnOpen.pack(side="left", anchor="sw", expand=True, fill="x")
+        btnRun = ttk.Button(self, text="Run", command=lambda: SortDownloads.run())
+        btnRun.pack(side="left", anchor="s", expand=True, fill="x")
+        btnClose = ttk.Button(self, text="Exit", command=lambda: controller.destroy())
+        btnClose.pack(side="left", anchor="se", expand=True, fill="x")
+        update_label(label1)
+        
+class ConfigPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text ="Config Page", font=LARGE_FONT)
+        label.pack(pady=10)
+
+        btnSave = ttk.Button(self, text="Save")
+        btnSave.pack(side="left", anchor="sw", expand=True, fill="x")
+        btnBack = ttk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage))
+        btnBack.pack(side="left", anchor="se", expand=True, fill="x")
+
+def update_label(label):
+    label.configure(text="Updated Label")
 
 app = ConfigureTool()
+app.geometry("400x100")
 app.mainloop()
--- Scripts/SortDownloads.py
+++ Scripts/SortDownloads.py
@@ -1,97 +1,100 @@
 import os, shutil, json
 from win10toast import ToastNotifier
 
+def run():
+    # Create folder in users app data to store config file
+    appdataPath = os.getenv('APPDATA')
+    if not os.path.exists(appdataPath + "\\Automated-File-Manager"):
+        os.makedirs(appdataPath + "\\Automated-File-Manager")
+    appdataPath += "\\Automated-File-Manager\\"
 
-# Create folder in users app data to store config file
-appdataPath = os.getenv('APPDATA')
-if not os.path.exists(appdataPath + "\\Automated-File-Manager"):
-    os.makedirs(appdataPath + "\\Automated-File-Manager")
-appdataPath += "\\Automated-File-Manager\\"
-
-# Default config JSON
-configData = {
-    "folderName": ["Text Files", "Zips", "Executables", "Pictures", "Documents", "Other"],
-    "docExtensions": [".doc", ".docx", ".pptx", ".pdf", ".xlsx", ".vsdx"],
-    "exeExtensions": [".exe", ".msi"],
-    "picExtensions": [".png", ".jpg"],
-    "txtExtensions": [".txt", ".json"],
-    "zipExtensions": [".zip", ".rar", ".7z", ".gz"],
-    "folderPaths": {
-        "docPath": "Documents/",
-        "exePath": "Executables/",
-        "picPath": "Pictures/",
-        "txtPath": "Text Files/",
-        "zipPath": "Zips/",
-        "otherPath": "Other/"
+    # Default config JSON
+    configData = {
+        "folderName": ["Text Files", "Zips", "Executables", "Pictures", "Documents", "Other"],
+        "docExtensions": [".doc", ".docx", ".pptx", ".pdf", ".xlsx", ".vsdx"],
+        "exeExtensions": [".exe", ".msi"],
+        "picExtensions": [".png", ".jpg"],
+        "txtExtensions": [".txt", ".json"],
+        "zipExtensions": [".zip", ".rar", ".7z", ".gz"],
+        "folderPaths": {
+            "docPath": "Documents/",
+            "exePath": "Executables/",
+            "picPath": "Pictures/",
+            "txtPath": "Text Files/",
+            "zipPath": "Zips/",
+            "otherPath": "Other/"
+        }
     }
-}
 
-# Check if config exists. If it doesn't, write configData to new file
-if not os.path.exists(appdataPath + "config.json"):
-    with open(appdataPath + "config.json", "w") as outfile:
-        json.dump(configData, outfile, indent=4)
+    # Check if config exists. If it doesn't, write configData to new file
+    if not os.path.exists(appdataPath + "config.json"):
+        with open(appdataPath + "config.json", "w") as outfile:
+            json.dump(configData, outfile, indent=4)
 
-# Loads the config file
-with open(appdataPath + "config.json") as jsonFile:
-    config = json.load(jsonFile)
+    # Loads the config file
+    with open(appdataPath + "config.json") as jsonFile:
+        config = json.load(jsonFile)
 
-path = os.path.expanduser("~\Downloads\\")
-folderName = config["folderName"]
-docExtensions = config["docExtensions"]
-exeExtensions = config["exeExtensions"]
-picExtensions = config["picExtensions"]
-txtExtensions = config["txtExtensions"]
-zipExtensions = config["zipExtensions"]
-folderPaths = config["folderPaths"]
+    path = os.path.expanduser("~\Downloads\\")
+    folderName = config["folderName"]
+    docExtensions = config["docExtensions"]
+    exeExtensions = config["exeExtensions"]
+    picExtensions = config["picExtensions"]
+    txtExtensions = config["txtExtensions"]
+    zipExtensions = config["zipExtensions"]
+    folderPaths = config["folderPaths"]
 
-numFiles = 0
-items = os.listdir(path)
+    numFiles = 0
+    items = os.listdir(path)
 
-def moveFile(file):
-    for extension in docExtensions:
-        if extension in file and not os.path.exists(path + folderPaths["docPath"] + file):
-            shutil.move(path + file, path + folderPaths["docPath"] + file)
+    def moveFile(file):
+        for extension in docExtensions:
+            if extension in file and not os.path.exists(path + folderPaths["docPath"] + file):
+                shutil.move(path + file, path + folderPaths["docPath"] + file)
+                return
+        for extension in exeExtensions:
+            if extension in file and not os.path.exists(path + folderPaths["exePath"] + file):
+                shutil.move(path + file, path + folderPaths["exePath"] + file)
+                return
+        for extension in picExtensions:
+            if extension in file and not os.path.exists(path + folderPaths["picPath"] + file):
+                shutil.move(path + file, path + folderPaths["picPath"] + file)
+                return
+        for extension in txtExtensions:
+            if extension in file and not os.path.exists(path + folderPaths["txtPath"] + file):
+                shutil.move(path + file, path + folderPaths["txtPath"] + file)
+                return
+        for extension in zipExtensions:
+            if extension in file and not os.path.exists(path + folderPaths["zipPath"] + file):
+                shutil.move(path + file, path + folderPaths["zipPath"] + file)
+                return
+        # If none of above, move to Other folder
+        if not os.path.exists(path + folderPaths["otherPath"] + file):
+            shutil.move(path + file, path + folderPaths["otherPath"] + file)
             return
-    for extension in exeExtensions:
-        if extension in file and not os.path.exists(path + folderPaths["exePath"] + file):
-            shutil.move(path + file, path + folderPaths["exePath"] + file)
-            return
-    for extension in picExtensions:
-        if extension in file and not os.path.exists(path + folderPaths["picPath"] + file):
-            shutil.move(path + file, path + folderPaths["picPath"] + file)
-            return
-    for extension in txtExtensions:
-        if extension in file and not os.path.exists(path + folderPaths["txtPath"] + file):
-            shutil.move(path + file, path + folderPaths["txtPath"] + file)
-            return
-    for extension in zipExtensions:
-        if extension in file and not os.path.exists(path + folderPaths["zipPath"] + file):
-            shutil.move(path + file, path + folderPaths["zipPath"] + file)
-            return
-    # If none of above, move to Other folder
-    if not os.path.exists(path + folderPaths["otherPath"] + file):
-        shutil.move(path + file, path + folderPaths["otherPath"] + file)
-        return
 
-    #TODO: Don't count duplicate files
-    #numFiles -= 1
-    #NOTE: If a file already exists, it gets put in Other.
-    # What if that file also exists in other?
+        #TODO: Don't count duplicate files
+        #numFiles -= 1
+        #NOTE: If a file already exists, it gets put in Other.
+        # What if that file also exists in other?
 
-# Creates new folder if it doesn't already exist in path
-for folder in folderName:
-    if not os.path.exists(path + folder):
-        os.makedirs(path + folder)
+    # Creates new folder if it doesn't already exist in path
+    for folder in folderName:
+        if not os.path.exists(path + folder):
+            os.makedirs(path + folder)
 
-# Check each file type and move accordingly
-for file in items:
-    # Avoid category folders & desktop ini file
-    if file in folderName or file == "desktop.ini":
-        continue
-    # Move the file
-    numFiles += 1
-    moveFile(file)
+    # Check each file type and move accordingly
+    for file in items:
+        # Avoid category folders & desktop ini file
+        if file in folderName or file == "desktop.ini":
+            continue
+        # Move the file
+        numFiles += 1
+        moveFile(file)
 
-#TODO: Move this to GUI, remove notifs
-toaster = ToastNotifier()
-toaster.show_toast("Daily Automation", f"Organized {numFiles} files in Downloads.", threaded=True, icon_path=None, duration=6)
+    #TODO: Move this to GUI, remove notifs
+    toaster = ToastNotifier()
+    toaster.show_toast("Daily Automation", f"Organized {numFiles} files in Downloads.", threaded=True, icon_path=None, duration=6)
+
+if __name__ == "__main__":
+    run()
