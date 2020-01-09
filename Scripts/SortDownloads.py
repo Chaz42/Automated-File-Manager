d2f25a32e214b9b50618fdbd4b9457ecaeddc6f8
Chaz42 <forteetwo@hotmail.com>
2020-01-09 02:22:42 -0500

    Added json config, started GUI

--- Scripts/SortDownloads.py
+++ Scripts/SortDownloads.py
@@ -1,51 +1,55 @@
-import os, shutil
+import os, shutil, json
 from win10toast import ToastNotifier
 
-path = "C:/Users/forte/Downloads/"
-folderName = ["Text Files", "Zips", "Executables", "Pictures", "Documents", "Other"]
-docExtensions = [".doc", ".docx", ".pptx", ".pdf", ".xlsx", ".vsdx"]
-exeExtensions = [".exe", ".msi"]
-picExtensions = [".png", ".jpg"]
-txtExtensions = [".txt", ".json"]
-zipExtensions = [".zip", ".rar", ".7z", ".gz"]
-docPath = "Documents/"
-exePath = "Executables/"
-picPath = "Pictures/"
-txtPath = "Text Files/"
-zipPath = "Zips/"
-otherPath = "Other/"
+#TODO: Check if first launch, if so: create default config file, place in Appdata
+# if not, load file from appdata
+with open("c:/Users/forte/Documents/github/Automated-File-Manager/Scripts/config.json") as jsonFile:
+    config = json.load(jsonFile)
+
+path = os.path.expanduser("~\Downloads\\")
+folderName = config["folderName"]
+docExtensions = config["docExtensions"]
+exeExtensions = config["exeExtensions"]
+picExtensions = config["picExtensions"]
+txtExtensions = config["txtExtensions"]
+zipExtensions = config["zipExtensions"]
+folderPaths = config["folderPaths"]
+
 numFiles = 0
 numFiles += len(folderName)
 items = os.listdir(path)
 
+
 def moveFile(file):
     for extension in docExtensions:
-        if extension in file and not os.path.exists(path + docPath + file):
-            shutil.move(path + file, path + docPath + file)
+        if extension in file and not os.path.exists(path + folderPaths["docPath"] + file):
+            shutil.move(path + file, path + folderPaths["docPath"] + file)
             return
     for extension in exeExtensions:
-        if extension in file and not os.path.exists(path + exePath + file):
-            shutil.move(path + file, path + exePath + file)
+        if extension in file and not os.path.exists(path + folderPaths["exePath"] + file):
+            shutil.move(path + file, path + folderPaths["exePath"] + file)
             return
     for extension in picExtensions:
-        if extension in file and not os.path.exists(path + picPath + file):
-            shutil.move(path + file, path + picPath + file)
+        if extension in file and not os.path.exists(path + folderPaths["picPath"] + file):
+            shutil.move(path + file, path + folderPaths["picPath"] + file)
             return
     for extension in txtExtensions:
-        if extension in file and not os.path.exists(path + txtPath + file):
-            shutil.move(path + file, path + txtPath + file)
+        if extension in file and not os.path.exists(path + folderPaths["txtPath"] + file):
+            shutil.move(path + file, path + folderPaths["txtPath"] + file)
             return
     for extension in zipExtensions:
-        if extension in file and not os.path.exists(path + zipPath + file):
-            shutil.move(path + file, path + zipPath + file)
+        if extension in file and not os.path.exists(path + folderPaths["zipPath"] + file):
+            shutil.move(path + file, path + folderPaths["zipPath"] + file)
             return
     # If none of above, move to Other folder
-    if not os.path.exists(path + otherPath + file):
-        shutil.move(path + file, path + otherPath + file)
+    if not os.path.exists(path + folderPaths["otherPath"] + file):
+        shutil.move(path + file, path + folderPaths["otherPath"] + file)
         return
 
     #TODO: Don't count duplicate files
     #numFiles -= 1
+    #NOTE: If a file already exists, it gets put in Other.
+    # What if that file also exists in other?
 
 # Creates new folder if it doesn't already exist in path
 for folder in folderName:
@@ -63,4 +67,4 @@ for file in items:
     moveFile(file)
 
 toaster = ToastNotifier()
-toaster.show_toast("Daily Automation", "Organized " + str(numFiles - len(folderName)) + " files in Downloads.", threaded=True, icon_path=None, duration=6)
+toaster.show_toast("Daily Automation", f"Organized {numFiles - len(folderName)} files in Downloads.", threaded=True, icon_path=None, duration=6)
